66060557d43382ebe57d1130c1928e8f883af203
Sobyt <kravczov.andrian@mail.ru>
2023-05-07 04:14:45 +0300

    feat: finish 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const { stdout } = process;
+const path = require('path');
+const fs = require('fs');
+const filePath = path.join(__dirname, 'text.txt')
+const stream = fs.createReadStream(filePath, 'utf-8')
+let result = ''
+stream.on('data', (data) => result += data)
+stream.on('end', () => stdout.write(result))
+stream.on('error', (error) => console.log(`Error`, error.message))

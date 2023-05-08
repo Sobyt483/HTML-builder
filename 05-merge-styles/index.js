66cc66ae180a0078fd288c7c5bd2eb36e172681e
Sobyt <kravczov.andrian@mail.ru>
2023-05-08 04:18:44 +0300

    feat: finish tasks 4 and 5

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css')
+const stylesPath = path.join(__dirname, 'styles')
+const writeStream = fs.createWriteStream(bundlePath)
+const options = { withFileTypes: true }
+
+fs.promises.readdir(stylesPath, options).then((data) => {
+  data.forEach((el) => {
+    if (path.extname(el.name) === '.css') {
+      let result = []
+      const readStream = fs.createReadStream(path.join(stylesPath, el.name), 'utf-8')
+      readStream.on('data', (data) => result.push(data))
+      readStream.on('end', () => writeStream.write(result.join('')))
+    }
+  })
+})

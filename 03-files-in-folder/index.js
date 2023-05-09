d8147b88134a8dde602b6c6bb82d50c4f0282db5
Sobyt <kravczov.andrian@mail.ru>
2023-05-10 01:19:58 +0300

    fix: fix 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,9 +7,8 @@ const options = {withFileTypes: true};
 fs.promises.readdir(dirPath, options).then((data) => {
   data.forEach((el) => {
     if(el.isFile()){
-      const arr = el.name.split('.');
-      const name = arr[0];
-      const extn = arr[1];
+      const name = el.name.replace(path.extname(el.name), '');
+      const extn = path.extname(el.name).slice(1);
       fs.stat(path.join(dirPath, el.name), (error, stat) =>  console.log(`${name} - ${extn} - ${stat.size}`));
     }
   });
